046f3ad3dd2356beebde554692b557c0a68c8366
Connor Zint <czint123@gmail.com>
2018-07-19 16:47:50 -0700

    add connector property to mock db

--- server/boot/05-tables.js
+++ server/boot/05-tables.js
@@ -13,15 +13,15 @@ module.exports = (server) => {
   // loopback sometimes throws warnings when dealing with large amounts of tables and relations
   db.setMaxListeners(TABLES.length * 2);
   db.connector.execute('CREATE SCHEMA IF NOT EXISTS PlanSchema', (err, results) => {
-      db.isActual(TABLES, (err, isActual) => {
+    db.isActual(TABLES, (err, isActual) => {
+      if (err) return callback(err);
+      if (isActual) {
+          console.log('Tables found .. skipping creation');
+      }
+      db.autoupdate(TABLES, (err) => {
           if (err) return callback(err);
-          if (isActual) {
-              console.log('Tables found .. skipping creation');
-          }
-          db.autoupdate(TABLES, (err) => {
-              if (err) return callback(err);
-              console.log('Tables created');
-          });
+          console.log('Tables created');
       });
+    });
   });
 };
--- server/test/mocks/MockDb.js
+++ server/test/mocks/MockDb.js
@@ -3,27 +3,33 @@ module.exports = class MockDb {
 		this.name = name || 'db';
 		this.tables = {};
 		this.maxListeners = 1;
-		if (tables) {
-			for (const table of tables) {
-				this.tables[table] = {};
-			}
-		}
+    this.connector = {
+      execute: (dbQuery, callback) => {
+        callback(false, {})
+      }
+    }
+
+    if (tables) {
+      for (const table of tables) {
+        this.tables[table] = {};
+      }
+    }
 	}
-	
+
 	isActual(tables, cb) {
 		for (const table of tables) {
 			if (!this.tables[table]) return cb(null, false);
 		}
 		return cb(null, true);
 	}
-	
+
 	autoupdate(tables, cb) {
 		for (const table of tables) {
 			if (!this.tables[table]) this.tables[table] = {};
 		}
 		return cb(null);
 	}
-	
+
 	setMaxListeners(number) {
 		this.maxListeners = number;
 	}
